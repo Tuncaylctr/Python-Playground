d45c5d975352e9c48c3a81f965bd4e86d33d0cc2
Tuncaylctr <tunjay.huseynli@gmail.com>
2026-05-23 10:00:00 +0300

    smtplib&datetime

---
+++ project-birthday/main.py
@@ -0,0 +1,55 @@
+# import smtplib
+
+# my_email= "shoponline787878@gmail.com"
+# password = ""
+
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(
+#         from_addr=my_email,
+#         to_addrs="tuncaylctr@yahoo.com",
+#         msg="Subject:Hello!\n\nkenangotdu!")
+
+# import datetime as dt
+# now = dt.datetime.now()
+# year = now.year
+# month = now.month
+# day_of_week = now.weekday()
+# print(day_of_week)
+
+# date_of_birth = dt.datetime(year = 2006 , month = 9 , day = 26)
+# print(date_of_birth)
+# import random
+# import datetime as dt
+# import smtplib
+# now = dt.datetime.now()
+# current_day = now.weekday()
+# my_email= "shoponline787878@gmail.com"
+# password = ""
+# print(current_day)
+
+# if current_day == 4:
+#     with open('quotes.txt', 'r') as quotes:
+#         lines = quotes.readlines()
+#         quote_of_day = random.choice(lines)
+#         # x = random.randint(0,101)
+#         # quote_of_day = lines[x]
+
+#     with smtplib.SMTP("smtp.gmail.com") as connection:
+#         connection.starttls()
+#         connection.login(user=my_email, password=password)
+#         connection.sendmail(
+#             from_addr=my_email,
+#             to_addrs="tuncaylctr@yahoo.com",
+#             msg=f"Subject:Friday motivation\n\n{quote_of_day}")
+
+
+
+my_email= "shoponline787878@gmail.com"
+password = "hjsmwshbpdqslmnw"
+        
+
+
+
+
