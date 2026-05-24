77f8923c21b2254c7a573cee724a10899410fc58
Tuncaylctr <tunjay.huseynli@gmail.com>
2026-05-24 12:00:00 +0300

    birthday-project

--- project-flash-card/main.py
+++ project-flash-card/main.py
@@ -1,5 +1,3 @@
-
-
 from tkinter import *
 import pandas
 import random
@@ -19,7 +17,7 @@ else:
 def next_card():
     global current_card , flip_timer
     window.after_cancel(flip_timer)
-    current_card= random.choice(to_learn)
+    current_card = random.choice(to_learn)
     canvas.itemconfig(card_title, text="French", fill="black")
     canvas.itemconfig(card_text,text= current_card["French"], fill= "black")
     canvas.itemconfig(card_background, image= card_front_img)
